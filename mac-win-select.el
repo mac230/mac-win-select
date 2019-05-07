89baf7f17f3a48ff6a7fde79fa26c01aee509487
Mahlon Collins <mahlon@umn.edu>
2019-05-06 21:26:53 -0600

    Initial commit.

---
+++ mac-win-select.el
@@ -0,0 +1,6 @@
+;; mac-win-select     -*- mode: org; fill-column: 70; eval: (auto-fill-mode) -*-
+
+* mac-win-select: My function for selecting windows in emacs
+This function allows rapid window selection based on a simple key
+presses.  It is conceptually similar to 'ace-window', but customized
+to my default window setup.
